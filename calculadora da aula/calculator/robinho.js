843a0a80d6a354a7666baf491bfbd82946272cfe
RonilsonFeliz <105285021+RonilsonFeliz@users.noreply.github.com>
2022-05-11 09:40:48 -0300

    Add files via upload

---
+++ calculadora da aula/calculator/robinho.js
@@ -0,0 +1,8 @@
+alert("Tuts tuts quero ver")//notificação de interação na página
+function mudartextoeacao()
+{
+    document.getElementById("botao").innerHTML = "" //aqui estamos modificando o botão via Id
+    alert("Mentira bobo")
+    createImageBitmap
+}
+
