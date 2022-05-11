2f4b265bcba1510a38e6025603f56cd90c0d5283
RonilsonFeliz <105285021+RonilsonFeliz@users.noreply.github.com>
2022-05-11 08:00:38 -0300

    Add files via upload

---
+++ calculadora da aula/js/calculadora.js
@@ -0,0 +1,8 @@
+alert("Explodirei em 48 horas")//notificação de interação na página
+function mudartextoeacao()
+{
+    document.getElementById("botao").innerHTML = "24 horas restantes" //aqui estamos modificando o botão vai Id
+    alert("ira explodir já já")
+    createImageBitmap
+}
+
